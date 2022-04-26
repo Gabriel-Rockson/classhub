4516f550e800be30393917dc47171d507c128b15
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-26 12:58:14 +0000

    Add custom user model

--- STUDENTSATTENDANCE/config/settings/base.py
+++ STUDENTSATTENDANCE/config/settings/base.py
@@ -16,6 +16,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # developer created apps
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -89,3 +92,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
