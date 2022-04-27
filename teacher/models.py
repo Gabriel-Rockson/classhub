cf0d34a98fec773431888feca0082f5be5d4a8f1
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-27 16:31:06 +0000

    Create the Teacher model

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Teacher(models.Model):
+    """Model to represent individual teacher"""
+
+    first_name = models.CharField(
+        verbose_name=_("First Name"),
+        null=False,
+        blank=False,
+        max_length=50,
+        help_text="Enter the first name of the teacher.",
+    )
+    last_name = models.CharField(
+        verbose_name=_("Last Name"),
+        null=False,
+        blank=False,
+        max_length=50,
+        help_text="Enter the last name of the teacher.",
+    )
+    middle_name = models.CharField(
+        verbose_name=_("Middle Name"),
+        null=True,
+        blank=True,
+        max_length=50,
+        help_text="Enter middle name of teacher if applies.",
+    )
+    email = models.EmailField(
+        verbose_name=_("Email Address"),
+        null=False,
+        blank=False,
+        help_text="Email address of the teacher.",
+        max_length=255,
+    )
+    address = models.CharField(
+        verbose_name=_("Address of Teacher"), null=True, blank=True, max_length=100
+    )
+    cell_number = models.CharField(
+        verbose_name=_("Cell Number"),
+        null=False,
+        blank=False,
+        max_length=20,
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = "Teacher"
+        verbose_name_plural = "Teachers"
+        ordering = ("first_name",)
