768c9bbcfbd19d40b300f8596881f1b1384bb2a4
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-27 16:29:22 +0000

    Register django-extensions in the apps and setup SHELL_PLUS

--- STUDENTSATTENDANCE/config/settings/base.py
+++ STUDENTSATTENDANCE/config/settings/base.py
@@ -96,3 +96,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+SHELL_PLUS = "ipython"
--- STUDENTSATTENDANCE/config/settings/dev.py
+++ STUDENTSATTENDANCE/config/settings/dev.py
@@ -5,13 +5,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+INSTALLED_APPS += [
+    "django_extensions",
+]
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'students_attendance_db',
-        'USER': 'hotice',
-        'PASSWORD': 'testpass1234',
-        'HOST': 'localhost',
-        'PORT': 5432
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "students_attendance_db",
+        "USER": "hotice",
+        "PASSWORD": "testpass1234",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
