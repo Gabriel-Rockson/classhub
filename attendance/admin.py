86342d0fece193457a727cc2479a7b64e2aef2e9
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-27 16:31:30 +0000

    Register the student model and the teacher model on the admin interface

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Class
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ["grade"]
