53ddc48e8bd029a817efad28d111dbf6096177ba
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-27 16:30:18 +0000

    Create the model for the class and the StudentAttendance

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Class(models.Model):
+    """Model to represent individual classes"""
+
+    class Grades(models.TextChoices):
+        ONE = 1, _("1")
+        TWO = 2, _("2")
+        THREE = 3, _("3")
+        FOUR = 4, _("4")
+        FIVE = 5, _("5")
+        SIX = 6, _("6")
+        SEVEN = 7, _("7")
+        EIGHT = 8, _("8")
+        NINE = 9, _("9")
+        TEN = 10, _("10")
+        ELEVEN = 11, _("11")
+        TWELVE = 12, _("12")
+        THIRTEEN = 13, _("13")
+        FOURTEEN = 14, _("14")
+        FIFTEEN = 15, _("15")
+        SIXTEEN = 16, _("16")
+
+    grade = models.CharField(
+        verbose_name=_("Grade"),
+        max_length=2,
+        help_text="Select grade of class.",
+        null=False,
+        blank=False,
+        choices=Grades.choices,
+        default=Grades.ONE,
+    )
+
+    def __str__(self):
+        return f"Grade {self.grade}"
+
+    class Meta:
+        verbose_name = "Class"
+        verbose_name_plural = "Classes"
+        ordering = ("grade",)
+
+
+class StudentAttendance(models.Model):
+    """Model to represent individual attendance record of a student"""
+
+    class AttendanceOptions(models.TextChoices):
+        PRESENT = "P", _("Present")
+        UNEX = "UNEX", _("SC-UNEX ( Unexcused / Unverified")
+        ETRD = "ETRD", _("SC-ETRD ( Excused Tardy )")
+        UTRD = "UTRD", _("SC-UTRD ( Unexcused Tardy )")
+        VTP = "VTP", _("SC-VTP ( Virtual Present Code )")
+
+    student = models.ForeignKey(
+        "student.Student",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="attendances",
+    )
+
+    attendance_created = models.DateTimeField(
+        _("Attendance Creation Date"), blank=False, null=False, auto_now_add=True
+    )
+    attendance_updated = models.DateTimeField(
+        _("Last Modified"), null=True, blank=True, auto_now=True
+    )
+    attendance = models.CharField(
+        verbose_name=_("Attendance"),
+        max_length=4,
+        null=False,
+        blank=False,
+        default=AttendanceOptions.PRESENT,
+        choices=AttendanceOptions.choices,
+    )
+
+    def __str__(self):
+        return f"Attendance on - {self.attendance_created}"
+
+
