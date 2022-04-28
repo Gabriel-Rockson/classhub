686348a1f32c34616eb871240f06ec3f2ffef2fd
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-28 00:19:58 +0000

    Modify database settings

--- STUDENTSATTENDANCE/config/settings/prod.py
+++ STUDENTSATTENDANCE/config/settings/prod.py
@@ -4,7 +4,9 @@ import dj_database_url
 
 from .base import *
 
-SECRET_KEY = os.environ.get("SECRET_KEY", default="hrhppn-&+r2+^n4siakw4)@8+ua@i^a(yokss$+ln@-a2z=v8j")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", default="hrhppn-&+r2+^n4siakw4)@8+ua@i^a(yokss$+ln@-a2z=v8j"
+)
 
 DEBUG = False
 
@@ -22,7 +24,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Databases
 DATABASES = {
     "default": dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default="postgres://students_attendance_user:tg16chbacahdDcvCoRqVrRN2qqiom7rx@dpg-c9ktesc41ls49b94eek0-a/students_attendance_db_up8o",
         conn_max_age=600,
     )
 }
