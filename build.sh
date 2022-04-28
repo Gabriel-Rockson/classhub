eeecf40372d1e1a3fa24546a6190e63a3b2c9db6
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-28 00:56:00 +0000

    Modify the build.sh

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@ set -o errexit
 cd frontend
 
 # Build the frontend
-npm install # install the packages
-npm run build # build the application
+# npm install # install the packages
+# npm run build # build the application
 
-# Move files at the build root inside a root subdirectory
-mkdir -p build/root
-for file in $(ls build | grep -E -v '^(index\.html|static|root)$'); do
-    mv "build/$file" build/root;
-done
+# # Move files at the build root inside a root subdirectory
+# mkdir -p build/root
+# for file in $(ls build | grep -E -v '^(index\.html|static|root)$'); do
+#     mv "build/$file" build/root;
+# done
 
-cd ..
+# cd ..
 
 poetry install 
 
