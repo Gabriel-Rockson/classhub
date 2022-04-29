70dc08ebfde56723915a367a079cc13bc5e637f5
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-29 12:17:24 +0000

    Add a catchall url

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from frontend import views
 
@@ -6,4 +6,5 @@ from frontend import views
 app_name = "frontend"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    re_path(r"^(?:.*)/?$", views.Index.as_view()),
 ]
