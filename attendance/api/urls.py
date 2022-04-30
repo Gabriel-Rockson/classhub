15ac19573f237bae636b046a7b03a40524c47d0a
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-04-30 09:25:05 +0000

    Add the url endpoints relating to the class

--- attendance/api/urls.py
+++ attendance/api/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from attendance.api.views import (
     StudentAttendanceListCreateAPIView,
     StudentAttendanceDetailAPIView,
+    ClassListCreateAPIView,
+    ClassDetailAPIView,
 )
 
 
 urlpatterns = [
     path("attendances/", StudentAttendanceListCreateAPIView.as_view()),
     path("attendances/<str:attendance_uid>/", StudentAttendanceDetailAPIView.as_view()),
+    path("classes/", ClassListCreateAPIView.as_view()),
+    path("classes/<str:class_uid>/", ClassDetailAPIView.as_view()),
 ]
