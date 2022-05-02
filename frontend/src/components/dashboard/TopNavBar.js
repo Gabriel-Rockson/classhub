0f909d2a0b1886a35f154d8c80cc5721d2eb20a2
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-02 15:04:04 +0000

    Add drop down menu to the top navbar

--- frontend/src/components/dashboard/TopNavBar.js
+++ frontend/src/components/dashboard/TopNavBar.js
@@ -16,6 +16,11 @@ import {
   DrawerContent,
   DrawerCloseButton,
   useDisclosure,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
 } from "@chakra-ui/react";
 
 import {
@@ -24,6 +29,7 @@ import {
   AiOutlineMenuUnfold,
   AiOutlineMenuFold,
 } from "react-icons/ai";
+import { CgProfile, CgLogOut } from "react-icons/cg";
 
 import SecondaryNavBarMobile from "./SecondaryNavBarMobile";
 
@@ -38,8 +44,8 @@ const SearchInput = () => {
         />
         <Input
           placeholder="Search ..."
-          borderColor={"blue.200"}
-          focusBorderColor="blue.600"
+          borderColor={"telegram.600"}
+          focusBorderColor="telegram.800"
           rounded={"none"}
         />
       </InputGroup>
@@ -66,9 +72,10 @@ const TopNavBar = () => {
     <>
       <Flex
         justifyContent={"space-between"}
-        py={[2, 3]}
+        py={[3]}
         px={[2, 4, 6]}
         backgroundColor="white"
+        borderBottom={["solid", "none"]}
       >
         <Flex alignItems={"center"}>
           {windowWidth < 768 &&
@@ -113,7 +120,7 @@ const TopNavBar = () => {
           )}
           <Heading
             fontSize={["lg", "xl", "xl"]}
-            color={"facebook.800"}
+            color={"telegram.900"}
             fontFamily={"fantasy"}
           >
             StudentsAttendance
@@ -122,20 +129,35 @@ const TopNavBar = () => {
         <Flex alignItems={"center"}>
           {windowWidth >= 768 && <SearchInput />}
 
-          <Flex
-            alignItems={"center"}
-            color="blue.400"
-            fontWeight={"semibold"}
-            cursor="pointer"
-          >
-            <Avatar
-              name="Dan Abrahmov"
-              size={"sm"}
-              src="https://bit.ly/dan-abramov"
-              mr={2}
-            />
-            <Text>Nancy</Text> <Icon as={AiFillCaretDown} />
-          </Flex>
+          {/* <Text>Nancy</Text> <Icon as={AiFillCaretDown} /> */}
+          <Menu>
+            <MenuButton>
+              <Flex
+                alignItems={"center"}
+                color="telegram.600"
+                fontWeight={"semibold"}
+                cursor="pointer"
+              >
+                <Avatar
+                  name="Dan Abrahmov"
+                  size={"sm"}
+                  src="https://bit.ly/dan-abramov"
+                  mr={2}
+                />
+                Nancy <Icon as={AiFillCaretDown} />
+              </Flex>
+            </MenuButton>
+            <MenuList
+              border={"1px"}
+              borderColor={"telegram.300"}
+              color={"telegram.900"}
+              fontWeight={"bold"}
+            >
+              <MenuItem icon={<CgProfile />}>Profile</MenuItem>
+              <MenuDivider />
+              <MenuItem icon={<CgLogOut />}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
       </Flex>
     </>
