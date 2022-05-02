bcdbdfb33ec03c561673acb7e73c99c70fa3a2ea
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-02 22:43:47 +0000

    Add modal which contains form to add a new user

--- frontend/src/dashboard/StudentList.js
+++ frontend/src/dashboard/StudentList.js
@@ -16,10 +16,25 @@ import {
   Th,
   Td,
   TableCaption,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  VStack,
 } from "@chakra-ui/react";
+import { DatePicker } from "@orange_digital/chakra-datepicker";
 
 import { MdPersonAdd } from "react-icons/md";
 import { IoEyeSharp } from "react-icons/io5";
+import { HStack } from "@chakra-ui/react";
 
 const data = {
   name: "Gabriel Rockson",
@@ -29,6 +44,8 @@ const data = {
   student_id: "20594513",
 };
 function StudentList() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const navigate = useNavigate();
 
   const handleClick = (student_uid) => {
@@ -53,12 +70,129 @@ function StudentList() {
             _hover={{ backgroundColor: "facebook.800" }}
             _active={{ backgroundColor: "facebook.800" }}
             shadow={"md"}
+            onClick={onOpen}
           >
             <Flex alignItems={"center"}>
               <Icon fontSize={"xl"} mr={2} as={MdPersonAdd} /> Add Student
             </Flex>
           </Button>
         </Flex>
+
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          scrollBehavior={"inside"}
+          closeOnOverlayClick={false}
+        >
+          <ModalOverlay />
+          <ModalContent borderRadius={0}>
+            <ModalHeader>Add Student</ModalHeader>
+            <ModalCloseButton style={{ boxShadow: "none" }} color="red" />
+
+            <ModalBody>
+              <VStack spacing={5}>
+                <FormControl>
+                  <FormLabel>First Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Middle Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Last Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Date of Birth</FormLabel>
+                  {/* TODO replace this date time picker with a custom component that fits chakra or a package */}
+                  <Input type="date" borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Gender</FormLabel>
+                  <Select borderRadius={0}>
+                    <option value="M">Male</option>
+                    <option value="F">Female</option>
+                    <option value="O">Other</option>
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Race</FormLabel>
+                  <Select borderRadius={0}>
+                    <option value="W">White</option>
+                    <option value="B">Black / African American</option>
+                    <option value="AIN">American Indian</option>
+                    <option value="H">Native Hawaiian</option>
+                    <option value="O">Some Other Race</option>
+                  </Select>
+                </FormControl>
+                {/* TODO the grade of the student should be automatically filled based on the class list being viewed */}
+                <FormControl>
+                  <FormLabel>Student ID</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Address</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Father's Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Father's Contact</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Mother's Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Mother's Contact</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Guardian's Name</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Guardian's Email</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Home Phone</FormLabel>
+                  <Input borderRadius={0} />
+                </FormControl>
+              </VStack>
+            </ModalBody>
+
+            <ModalFooter>
+              <HStack spacing={5}>
+                <Button
+                  color="white"
+                  borderRadius={0}
+                  backgroundColor={"green.600"}
+                  style={{ boxShadow: "none" }}
+                  _hover={{ backgroundColor: "green.800" }}
+                  _active={{ backgroundColor: "green.800" }}
+                >
+                  Add Student
+                </Button>
+                <Button
+                  color="white"
+                  borderRadius={0}
+                  backgroundColor={"red.600"}
+                  style={{ boxShadow: "none" }}
+                  _hover={{ backgroundColor: "red.800" }}
+                  _active={{ backgroundColor: "red.800" }}
+                  onClick={onClose}
+                >
+                  Cancel
+                </Button>
+              </HStack>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
 
         <Text as={"small"} color={"telegram.900"} fontWeight={"bold"}>
           Swipe on the table to reveal more data
