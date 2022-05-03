d39ee3fa7195f15de182fd8b7b6e453aded07881
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-03 05:16:03 +0000

    Move files to src folder

---
+++ frontend/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <>
+      <Box>
+        <Heading color="blue.600">This is the HomePage</Heading>
+        <Link to="/app/dashboard">Dashboard</Link>
+      </Box>
+    </>
+  );
+}
+
+export default HomePage;
