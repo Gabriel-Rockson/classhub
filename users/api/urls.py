8f5b56769bda06594a371ca9b78157652e9ef937
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-04 23:04:27 +0000

    Add url endpoints for the user api views

---
+++ users/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from users.api.views import (
+    LoginAPIView,
+    RefreshTokenAPIView,
+    RegistrationAPIView,
+    UserListAPIView,
+    UserDetailAPIView,
+)
+
+
+urlpatterns = [
+    path("auth/login", LoginAPIView.as_view(), name="auth-login"),
+    path("auth/register", RegistrationAPIView.as_view(), name="auth-register"),
+    path("auth/refresh", RefreshTokenAPIView.as_view(), name="auth-refresh"),
+    path("users/", UserListAPIView.as_view()),
+    path("users/<str:user_uid>/", UserDetailAPIView.as_view()),
+]
