47912b3873d18abf65beebda066966fae0ec25ca
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-04 21:51:58 +0000

    Add custom user fields and custom user manager

--- users/models.py
+++ users/models.py
@@ -1,6 +1,72 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.utils.translation import gettext_lazy as _
+import uuid
+from django.utils import timezone
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+    def create_user(self, username=None, email=None, password=None, **kwargs):
+        if username is None:
+            raise ValueError("Username is required")
+        if password is None:
+            raise ValueError("Password is required")
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_teacher_user(self, username=None, email=None, password=None, **kwargs):
+        user = self.create_user(
+            username=username, email=email, password=password, **kwargs
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+
+    def create_superuser(self, username=None, email=None, password=None, **kwargs):
+
+        user = self.create_user(
+            username=username, email=email, password=password, **kwargs
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_uid = models.UUIDField(
+        verbose_name=_("User Uid"),
+        null=False,
+        blank=False,
+        db_index=True,
+        unique=True,
+        editable=False,
+        default=uuid.uuid4,
+    )
+    username = models.CharField(
+        verbose_name=_("Username"), null=False, blank=False, max_length=50, unique=True
+    )
+    email = models.EmailField(
+        verbose_name=_("Email"), null=True, blank=True, max_length=255, unique=True
+    )
+    is_staff = models.BooleanField(
+        verbose_name=_("Staff Status"),
+        default=False,
+    )
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "username"
+    
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
