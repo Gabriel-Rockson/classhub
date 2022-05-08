2d822c89cc64ec408d18c3ac4aac32de045fbcc5
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-08 12:50:42 +0000

    Add permission classes to the endpoints

--- student/api/views.py
+++ student/api/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from student.api.serializers import StudentSerializer
 from student.models import Student
 
 
 class StudentListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated,]
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 
 class StudentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated,]
     lookup_field = "student_uid"
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
