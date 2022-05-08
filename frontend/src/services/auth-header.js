8afd4bac79ffccac0a1e13df4945bfa2cf7f187b
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-08 18:57:06 +0000

    Use the accessToken instead of user

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -1,11 +1,11 @@
 import AuthService from "./auth.service";
 
 export default function authHeader() {
-  const user = AuthService.getCurrentUser();
+  const accessToken = AuthService.getAccessToken();
 
-  if (user) {
+  if (accessToken) {
     return {
-      Authorization: `Bearer ${user.access}`,
+      Authorization: `Bearer ${accessToken}`,
     };
   } else {
     return {};
