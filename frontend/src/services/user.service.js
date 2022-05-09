3569974e35bb6fee8ca563b95b09b0a27b65747d
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-09 11:18:45 +0000

    Add function to fetch the user's data

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,1 +1,15 @@
 // Services to handle fetching user related data from the backend
+import axios_instance from "./axios.service";
+import AuthService from "./auth.service";
+
+const getUserData = async (id) => {
+  return await axios_instance(`users/${id}/`).then((response) =>
+    AuthService.setUser(response.data)
+  );
+};
+
+const UserService = {
+  getUserData,
+};
+
+export default UserService;
