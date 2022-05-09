0d64942eb941cafceb6d7f3803f7edb04ef0e9c9
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-09 00:26:49 +0000

    Add signal to create a teacher profile when a user object is created

---
+++ users/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from teacher.models import Teacher
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_teacher_profile(sender, **kwargs):
+    instance = kwargs.get("instance")
+    created = kwargs.get("created")
+    if created:
+        teacher_profile = Teacher.objects.create(user=instance)
+        teacher_profile.save()
