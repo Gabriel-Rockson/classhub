217fe84bc8ea10512be516983be8be9d8680c6ff
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-09 06:24:56 +0000

    Add service to make api call to the teacher update endpoint

---
+++ frontend/src/services/teacher.service.js
@@ -0,0 +1,19 @@
+import axios_instance from "./axios.service";
+
+const updateTeacherData = async (id, data) => {
+  return await axios_instance.patch(`teachers/${id}/`, {
+    first_name: data.first_name,
+    middle_name: data.middle_name,
+    last_name: data.last_name,
+    email: data.email,
+    grade: data.grade,
+    address: data.address,
+    cell_number: data.cell_number,
+  });
+};
+
+const TeacherService = {
+  updateTeacherData,
+};
+
+export default TeacherService;
