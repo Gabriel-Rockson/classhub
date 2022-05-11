fa0bf57edccb3d2bbe981384868ffac0273699f3
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-11 23:55:02 +0000

    Add attendance marking functionality

--- frontend/src/dashboard/AddAttendance.js
+++ frontend/src/dashboard/AddAttendance.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Chakra components
@@ -21,19 +21,66 @@ import {
   AlertDescription,
   AlertTitle,
   Spinner,
+  FormErrorMessage,
+  FormErrorIcon,
+  FormControl,
 } from "@chakra-ui/react";
 
+// Formik and Yup
+import { Formik, Field, Form } from "formik";
+import * as Yup from "yup";
+
 // Icons
-import { GiCheckMark } from "react-icons/gi";
 import { ImUserCheck } from "react-icons/im";
 import { AiOutlineRollback } from "react-icons/ai";
 
 // Custom hooks
 import { useGradeAndStudents } from "../hooks/custom-hooks";
 
+// Services
+import AttendanceService from "../services/attendance.service";
+
 const AddAttendance = () => {
   const navigate = useNavigate();
+  const [attendanceToday, setAttendanceToday] = useState(null);
   const { isFetching, students, grade } = useGradeAndStudents();
+
+  useEffect(() => {
+    AttendanceService.getTodayAttendances()
+      .then((response) => {
+        setAttendanceToday(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const checkAttendance = (isSubmitting, status, student) => {
+    return (
+      (!isSubmitting && status?.status === 201) ||
+      attendanceToday?.some((attendance) => attendance.student === student.id)
+    );
+  };
+
+  const attendanceValidationSchema = Yup.object().shape({
+    attendance: Yup.string().required("This field is required"),
+  });
+
+  const handleStudentAttendanceSubmit = (
+    data,
+    { setErrors, setSubmitting, setStatus }
+  ) => {
+    setSubmitting(true);
+    AttendanceService.addStudentAttendance(data)
+      .then((response) => {
+        setStatus(response);
+      })
+      .catch((error) => {
+        setErrors(error?.response.data);
+      })
+      .finally(() => setSubmitting(false));
+  };
 
   if (isFetching) {
     return (
@@ -102,43 +149,79 @@ const AddAttendance = () => {
               <Tr key={student.id}>
                 <Td>{`${student.first_name} ${student.middle_name} ${student.last_name}`}</Td>
                 <Td>{student.student_id}</Td>
-                <Td>
-                  <Select borderRadius={5}>
-                    <option value="P">Present</option>
-                    <option value="UNEX">
-                      SC-UNEX ( Unexcused / Unverified )
-                    </option>
-                    <option value="ETRD">SC-ETRD ( Excused Tardy )</option>
-                    <option value="UTRD">SC-UTRD ( Unexcused Tardy )</option>
-                    <option value="VTP">SC-VTP ( Virtual Present Code )</option>
-                  </Select>
-                </Td>
-                <Td>
-                  <Button
-                    colorScheme={"telegram"}
-                    borderRadius={5}
-                    style={{ boxShadow: "none" }}
-                  >
-                    <Flex alignItems="center">
-                      <Icon as={ImUserCheck} mr={2} /> Save
-                    </Flex>
-                  </Button>
-                </Td>
+
+                <Formik
+                  initialValues={{ attendance: "P", student: student.id }}
+                  validationSchema={attendanceValidationSchema}
+                  onSubmit={handleStudentAttendanceSubmit}
+                >
+                  {({ errors, isSubmitting, touched, status, submitForm }) => (
+                    <>
+                      <Td>
+                        <Form>
+                          <FormControl
+                            isInvalid={
+                              !!errors.attendance && touched.attendance
+                            }
+                          >
+                            <Field
+                              as={Select}
+                              id="attendance"
+                              name="attendance"
+                              borderRadius={5}
+                              disabled={checkAttendance(
+                                isSubmitting,
+                                status,
+                                student
+                              )}
+                            >
+                              <option value="P">Present</option>
+                              <option value="UNEX">
+                                SC-UNEX ( Unexcused / Unverified )
+                              </option>
+                              <option value="ETRD">
+                                SC-ETRD ( Excused Tardy )
+                              </option>
+                              <option value="UTRD">
+                                SC-UTRD ( Unexcused Tardy )
+                              </option>
+                              <option value="VTP">
+                                SC-VTP ( Virtual Present Code )
+                              </option>
+                            </Field>
+                            <FormErrorMessage>
+                              <FormErrorIcon />
+                              {errors.attendance}
+                            </FormErrorMessage>
+                          </FormControl>
+                        </Form>
+                      </Td>
+                      <Td>
+                        <Button
+                          colorScheme={"telegram"}
+                          borderRadius={5}
+                          style={{ boxShadow: "none" }}
+                          onClick={submitForm}
+                          disabled={
+                            isSubmitting ||
+                            checkAttendance(isSubmitting, status, student)
+                          }
+                        >
+                          <Flex alignItems="center">
+                            <Icon as={ImUserCheck} mr={2} />{" "}
+                            {checkAttendance(isSubmitting, status, student)
+                              ? "Saved"
+                              : "Save"}
+                          </Flex>
+                        </Button>
+                      </Td>
+                    </>
+                  )}
+                </Formik>
               </Tr>
             ))}
           </Tbody>
         </Table>
-        <Flex justifyContent={"center"} py={2}>
-          <Button
-            borderRadius={5}
-            colorScheme={"telegram"}
-            style={{ boxShadow: "none" }}
-          >
-            <Flex alignItems={"center"}>
-              <Icon as={GiCheckMark} mr={2} /> Save Attendance
-            </Flex>
-          </Button>
-        </Flex>
       </TableContainer>
     </>
   );
