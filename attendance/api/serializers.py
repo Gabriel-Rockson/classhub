cd3b398590ac67dc914e7a2bd554e5a588af5d60
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-11 22:49:38 +0000

    Override the create method and raise a validation error if attendance is being created on the same twice

--- attendance/api/serializers.py
+++ attendance/api/serializers.py
@@ -1,4 +1,7 @@
-from attendance.models import StudentAttendance, Class
+from datetime import datetime
+
+from attendance.models import Class, StudentAttendance
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
 
@@ -26,6 +29,28 @@ class StudentAttendanceSerializer(serializers.ModelSerializer):
             "student": {"write_only": True},
             "attendance": {"write_only": True},
         }
+
+    def create(self, validated_data):
+        """
+        1. check if the attendance for today already exists for this student
+        2. raise validation error if attendance exists
+        """
+        student_attendance_today_exists = False
+        try:
+            student_attendance_today = StudentAttendance.objects.get(
+                student=validated_data["student"], created=datetime.today().date()
+            )
+            if student_attendance_today:
+                student_attendance_today_exists = True
+        except ObjectDoesNotExist as e:
+            pass
+        if student_attendance_today_exists:
+            raise serializers.ValidationError(
+                {"attendance": "Attendance for this student has been recorded today"}
+            )
+        instance = super(StudentAttendanceSerializer, self).create(validated_data)
+        instance.save()
+        return instance
 
 
 # NOTE - the class serializer is in student.api.serializers
