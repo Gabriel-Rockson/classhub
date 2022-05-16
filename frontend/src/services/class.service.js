13c3b4c15ee508d827eccd5c143fd4f09c760179
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-16 12:31:05 +0000

    Add api call to get the attendances of a particular class

--- frontend/src/services/class.service.js
+++ frontend/src/services/class.service.js
@@ -8,9 +8,14 @@ const getClassData = async (id) => {
   return await axios_instance.get(`classes/${id}/`);
 };
 
+const getGradeAttendaces = async (id) => {
+  return await axios_instance.get(`classes/attendances/${id}/`);
+};
+
 const ClassService = {
   getClassList,
   getClassData,
+  getGradeAttendaces,
 };
 
 export default ClassService;
