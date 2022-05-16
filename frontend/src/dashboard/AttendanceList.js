7cf707562d7f793fd6f68aa47d28d393f4dfa00f
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-16 12:33:34 +0000

    Fetch the class attendance and group them by date

--- frontend/src/dashboard/AttendanceList.js
+++ frontend/src/dashboard/AttendanceList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Chakra components
@@ -26,6 +26,9 @@ import { BsFillPatchQuestionFill } from "react-icons/bs";
 // Custom hooks
 import { useGradeAndStudents } from "../hooks/custom-hooks";
 
+// Services
+import ClassService from "../services/class.service";
+
 const data = {
   date: "Tue May 03 2022",
 };
@@ -42,12 +45,99 @@ const QuestionIcon = () => {
   );
 };
 
-// ! FIX - Warning: Function components cannot be given refs. Attempts to access this ref will fail. Did you mean to use React.forwardRef()?
 // ! This is the error that is preventing you from using tooltips or popovers, fix that and use popovers to show more information on the question marks
 
 const AllAttendances = () => {
   const navigate = useNavigate();
   const { grade } = useGradeAndStudents();
+  const [allAttendances, setAllAttendances] = useState(null);
+
+  useEffect(() => {
+    if (grade) {
+      ClassService.getGradeAttendaces(grade?.id)
+        .then((response) => setAllAttendances(response?.data))
+        .catch((error) => console.log(error));
+    }
+  }, [grade]);
+
+  // Function to group the attendances according to when they were created
+  const groupAttendances = (array, key) => {
+    return array?.reduce((groupedAttendances, currentAttendance) => {
+      (groupedAttendances[currentAttendance[key]] =
+        groupedAttendances[currentAttendance[key]] || []).push(
+        currentAttendance
+      );
+      return groupedAttendances;
+    }, {});
+  };
+
+  const rows = [];
+  const groupedAttendances = groupAttendances(allAttendances, "created");
+
+  if (allAttendances) {
+    for (let key in groupedAttendances) {
+      const attendances = groupedAttendances[key];
+
+      const groupedTypes = attendances.reduce((groupedTypes, attendance) => {
+        const {
+          PRESENT = 0,
+          UNEX = 0,
+          ETRD = 0,
+          UTRD = 0,
+          VTP = 0,
+        } = groupedTypes;
+        if (attendance.attendance === "P") {
+          return { ...groupedTypes, PRESENT: PRESENT + 1 };
+        } else if (attendance.attendance === "UNEX") {
+          return { ...groupedTypes, UNEX: UNEX + 1 };
+        } else if (attendance.attendance === "ETRD") {
+          return { ...groupedTypes, ETRD: ETRD + 1 };
+        } else if (attendance.attendance === "UTRD") {
+          return { ...groupedTypes, UTRD: UTRD + 1 };
+        } else if (attendance.attendance === "VTP") {
+          return { ...groupedTypes, VTP: VTP + 1 };
+        }
+      }, {});
+      rows.push(
+        <Tr key={key}>
+          <Td>{new Date(key).toDateString()}</Td>
+          <Td>{groupedTypes?.PRESENT || "-"}</Td>
+          <Td>{groupedTypes?.UNEX || "-"}</Td>
+          <Td>{groupedTypes?.ETRD || "-"}</Td>
+          <Td>{groupedTypes?.UTRD || "-"}</Td>
+          <Td>{groupedTypes?.VTP || "-"}</Td>
+          <Td>
+            <HStack spacing={4}>
+              <Button
+                borderRadius={5}
+                colorScheme={"telegram"}
+                style={{ boxShadow: "none" }}
+                size={"sm"}
+                onClick={() => handleClick(number)}
+              >
+                <Flex alignItems={"center"}>
+                  <Icon as={IoEyeSharp} mr={2} />
+                  view
+                </Flex>
+              </Button>
+              <Button
+                borderRadius={5}
+                colorScheme={"red"}
+                style={{ boxShadow: "none" }}
+                size={"sm"}
+                onClick={() => handleClick(number)}
+              >
+                <Flex alignItems={"center"}>
+                  <Icon as={MdDelete} mr={2} />
+                  delete
+                </Flex>
+              </Button>
+            </HStack>
+          </Td>
+        </Tr>
+      );
+    }
+  }
 
   const handleAddAttendance = () => {
     navigate("/app/dashboard/class-attendance/add-attendance");
@@ -110,46 +200,7 @@ const AllAttendances = () => {
               {/* TODO add tooltips to show the full of the code */}
             </Tr>
           </Thead>
-          <Tbody>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((number) => (
-              <Tr key={number}>
-                <Td>{data["date"]}</Td>
-                <Td>3</Td>
-                <Td>5</Td>
-                <Td>3</Td>
-                <Td>5</Td>
-                <Td>3</Td>
-                <Td>
-                  <HStack spacing={4}>
-                    <Button
-                      borderRadius={5}
-                      colorScheme={"telegram"}
-                      style={{ boxShadow: "none" }}
-                      size={"sm"}
-                      onClick={() => handleClick(number)}
-                    >
-                      <Flex alignItems={"center"}>
-                        <Icon as={IoEyeSharp} mr={2} />
-                        view
-                      </Flex>
-                    </Button>
-                    <Button
-                      borderRadius={5}
-                      colorScheme={"red"}
-                      style={{ boxShadow: "none" }}
-                      size={"sm"}
-                      onClick={() => handleClick(number)}
-                    >
-                      <Flex alignItems={"center"}>
-                        <Icon as={MdDelete} mr={2} />
-                        delete
-                      </Flex>
-                    </Button>
-                  </HStack>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
+          <Tbody>{rows}</Tbody>
         </Table>
       </TableContainer>
     </Box>
