9f637ede8174471c5387a2a90adb3caf2804f633
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-17 13:49:53 +0000

    Add stacked inline

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 
 from attendance.models import Grade, StudentAttendance
 from attendance.forms import GradeForm
+from student.models import Student
+
+
+class StudentsInline(admin.StackedInline):
+    model = Student
+    extra = 0
 
 
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
     form = GradeForm
     list_display = ["grade", "number_of_students"]
+    inlines = [
+        StudentsInline,
+    ]
 
 
 @admin.register(StudentAttendance)
