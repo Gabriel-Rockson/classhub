69af3abb0bee70ea9efb7ab10304619355b353f8
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-05-19 23:04:43 +0000

    Display the date's attendance detail in a tabular format

--- frontend/src/dashboard/AttendanceDetail.js
+++ frontend/src/dashboard/AttendanceDetail.js
@@ -2,7 +2,19 @@ import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 // Chakra components
-import { Flex, Button, Heading, Icon } from "@chakra-ui/react";
+import {
+  Flex,
+  Button,
+  Heading,
+  Icon,
+  TableContainer,
+  Table,
+  Tbody,
+  Thead,
+  Tr,
+  Th,
+  Td,
+} from "@chakra-ui/react";
 
 // Icons
 import { AiOutlineRollback } from "react-icons/ai";
@@ -10,10 +22,12 @@ import { AiOutlineRollback } from "react-icons/ai";
 // Custom hook
 import { useGradeAndStudents } from "../hooks/custom-hooks";
 
-export default function AttendanceDetail() {
+export default React.memo(function AttendanceDetail() {
   const { grade } = useGradeAndStudents();
   const navigate = useNavigate();
   const { state } = useLocation();
+
+  console.log(state.attendances);
 
   return (
     <>
@@ -38,11 +52,29 @@ export default function AttendanceDetail() {
         </Heading>
       </Flex>
 
-      <Flex my={10} justifyContent={"center"}>
-        <Heading fontSize={["lg", "xl", "2xl"]} color="yellow.800">
-          This feature is underway
-        </Heading>
-      </Flex>
+      <TableContainer py={4} boxShadow="lg" rounded="md">
+        <Table variant={"unstyled"}>
+          <Thead borderBottom={"2px"} borderColor={"gray.200"}>
+            <Tr>
+              <Th>Student Name</Th>
+              <Th>Date</Th>
+              <Th>Attendance Code</Th>
+              <Th>Attendance Name</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {state?.attendances?.map((attendance) => (
+              <Tr key={attendance.id}>
+                <Td>{attendance.student_name}</Td>
+                {/* TODO make the attendance name clickable, on click, take the user to the student's detail page, where they can view the student's full attendance details */}
+                <Td>{attendance.created}</Td>
+                <Td>{attendance.attendance}</Td>
+                <Td>{attendance.attendance_display}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </>
   );
-}
+});
