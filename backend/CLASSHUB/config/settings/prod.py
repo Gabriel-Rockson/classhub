9cd596bb1323b2562e957f98dc57d6657ac514c8
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-20 17:42:17 +0000

    Input the things needed to establish a connection with the database

--- backend/CLASSHUB/config/settings/prod.py
+++ backend/CLASSHUB/config/settings/prod.py
@@ -3,8 +3,6 @@ import django_on_heroku
 
 import dj_database_url
 
-# Configure django app for heroku
-django_on_heroku.settings(locals())
 
 from .base import *
 
@@ -14,7 +12,7 @@ SECRET_KEY = os.environ.get(
     "SECRET_KEY", default="hrhppn-&+r2+^n4siakw4)@8+ua@i^a(yokss$+ln@-a2z=v8j"
 )
 
-DEBUG = False
+DEBUG = True
 
 
 if IS_HEROKU:
@@ -35,7 +33,23 @@ CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
 # Database
 MAX_CONN_AGE = 600
 
-DATABASES = {'default': dj_database_url.config(
-    default=os.environ.get("DATABASE_URL"),
-    conn_max_age=600
-)}
+#DATABASES = {'default': dj_database_url.config(
+#    default=os.environ.get("DATABASE_URL"),
+#    conn_max_age=600
+#)}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "d40gtdt6jq0i4o",
+        "USER": "pocanzwppwbqkj",
+        "PASSWORD": "941e5c22a04896cbfc987927e99c8d4488f8a929d352827ef16b686e7aeffb50",
+        "HOST": "ec2-63-34-180-86.eu-west-1.compute.amazonaws.com",
+        "PORT": 5432,
+    }
+}
+
+
+# Configure django app for heroku
+django_on_heroku.settings(locals())
+
