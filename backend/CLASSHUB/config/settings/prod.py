11db1f9570d62538821ded086ccdb2ca954a9af6
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-08-20 17:24:51 +0000

    Change database settings

--- backend/CLASSHUB/config/settings/prod.py
+++ backend/CLASSHUB/config/settings/prod.py
@@ -35,4 +35,7 @@ CSRF_TRUSTED_ORIGINS = CORS_ORIGIN_WHITELIST
 # Database
 MAX_CONN_AGE = 600
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+DATABASES = {'default': dj_database_url.config(
+    default=os.environ.get("DATABASE_URL"),
+    conn_max_age=600
+)}
